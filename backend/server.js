0ecf7d70490c10830873a66892dbcb1be1607c88
Sanchay Chauhan <chauhan.sanchay19@gmail.com>
2023-12-19 20:04:29 +0530

    fdh

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URL;
-mongoose.connect(uri);
-const connection = mongoose.connection;
-connection.once("open", () => {
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
   console.log("MongoDB database connection established successfully");
-});
+}).catch((err)=>{
+console.log(err);
+})
 
 const usersRouter = require("./routes/users");
 app.use("/", usersRouter);
